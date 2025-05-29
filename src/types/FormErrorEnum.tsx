f19215fdc3336f5c29bcc5aa793364b79f3c0551
Adrian Mieńkowski <amienkowski@gmail.com>
2025-05-29 18:56:43 +0200

    Removed redundant enum file

--- src/types/FormErrorEnum.tsx
+++
@@ -1,6 +0,0 @@
-export enum FormError {
-    EmptyName = "Imiê nie mo¿e byæ puste",
-    EmptySurname = "Nazwisko nie mo¿e byæ puste",
-    EmptyEmail = "Adres email nie mo¿e byæ pusty",
-    EmptyMessage = "Uzupe³nij treœæ wiadomoœci"
-}
