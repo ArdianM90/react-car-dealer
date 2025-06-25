8917e4da8377242150f710e451281773f7c24d33
Adrian Mieńkowski <amienkowski@gmail.com>
2025-06-25 15:53:35 +0200

    vercel insights added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-﻿import { Button, Container } from 'react-bootstrap';
+﻿import {Button, Container} from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
+import {useEffect, useState} from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
-import { OfferContent } from './types/OfferContent';
-import { getRecommendedVehicles } from './service/MockApiService';
+import {SpeedInsights} from "@vercel/speed-insights/react";
+import {OfferContent} from './types/OfferContent';
+import {getRecommendedVehicles} from './service/MockApiService';
 import {RecommendationFrame} from "./components/RecommendationFrame.tsx";
 
 function App() {
@@ -16,9 +17,10 @@ function App() {
 
     return (
         <Container fluid>
+            <SpeedInsights/>
             <Row className="d-flex justify-content-end  mt-3 mb-3">
                 <Form className="search-bar d-flex mb-3">
-                    <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                    <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
                     <Button variant="outline-success">Search</Button>
                 </Form>
             </Row>
@@ -28,23 +30,25 @@ function App() {
             <div className="mb-3">
                 <p className="text-area">
                     Ten projekt powstał jako poligon doświadczalny do nauki i&nbsp;testowania komponentów oraz wzorców
-                    projektowych w&nbsp;React, a&nbsp;obecnie pełni rolę elementu portfolio. Zdjęcia samochodów użyte w&nbsp;tym
-                    projekcie pochodzą z&nbsp;serwisu Pexels (<a className="text-decoration-none" href="https://www.pexels.com">www.pexels.com</a>).
+                    projektowych w&nbsp;React, a&nbsp;obecnie pełni rolę elementu portfolio. Zdjęcia samochodów użyte
+                    w&nbsp;tym projekcie pochodzą z&nbsp;serwisu Pexels (
+                    <a className="text-decoration-none" href="https://www.pexels.com">www.pexels.com</a>).
                 </p>
                 <p className="text-area">
                     Strona została zainspirowana serwisami ogłoszeniowymi, takimi jak Otomoto. Szczególnie zachęcam
-                    do zapoznania się z&nbsp;podstronami zawierającymi listę ofert (z&nbsp;możliwością filtrowania) oraz kreatorem
-                    ogłoszeń, który umożliwia zarówno ich dodawanie, jak i&nbsp;edycję. Aplikacja korzysta z&nbsp;jednego komponentu
-                    pełniącego funkcję mock API, dzięki czemu z powodzeniem „udaje” działającą stronę z&nbsp;backendem.
+                    do zapoznania się z&nbsp;podstronami zawierającymi listę ofert (z&nbsp;możliwością filtrowania) oraz
+                    kreatorem ogłoszeń, który umożliwia zarówno ich dodawanie, jak i&nbsp;edycję. Aplikacja korzysta
+                    z&nbsp;jednego komponentu pełniącego funkcję mock API, dzięki czemu z powodzeniem „udaje” działającą
+                    stronę z&nbsp;backendem.
                 </p>
             </div>
-            <hr />
+            <hr/>
             <div className="fw-bold fs-4 text-primary text-start my-4">
                 Oferty wybrane dla Ciebie
             </div>
             <Row className="d-flex justify-content-between">
                 {offerCardList.map((item: OfferContent, index: number) =>
-                    <RecommendationFrame key={`card-${index}`} index={index} item={item} />
+                    <RecommendationFrame key={`card-${index}`} index={index} item={item}/>
                 )}
             </Row>
         </Container>
